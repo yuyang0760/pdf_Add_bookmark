0f87a4292749e1688ec659537e3ede4bcc94ef35
yy <1317030038@qq.com>
2021-06-23 16:55:58 +0800

    添加项目文件。

---
+++ 测试spirepdf/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace 测试spirepdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "创建pdf";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 99);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "删除现有书签";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(147, 15);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 55);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "添加书签到现有文档";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(147, 99);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(163, 50);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "添加书签";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 164);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ 测试spirepdf/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Pdf;
+using Spire.Pdf.Actions;
+using Spire.Pdf.Bookmarks;
+using Spire.Pdf.General;
+using Spire.Pdf.General.Find;
+using Spire.Pdf.Graphics;
+
+namespace 测试spirepdf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            PdfDocument doc = new PdfDocument();
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                PdfImage im = PdfImage.FromFile("00"+i.ToString()+".jpg");
+                float width = im.Width;
+                float height = im.Height;
+                PdfPageBase page = doc.Pages.Add(new SizeF(width, height), new PdfMargins(0, 0, 0, 0));
+                page.Canvas.DrawImage(im, 0, 0, width, height);
+            }
+
+            PdfImage im2 = PdfImage.FromFile("021.jpg");
+            float width2 = im2.Width;
+            float height2 = im2.Height;
+            PdfPageBase page2 = doc.Pages.Add(new SizeF(width2, height2), new PdfMargins(0, 0, 0, 0));
+            page2.Canvas.DrawImage(im2, 0, 0, width2, height2);
+
+
+            //Save pdf file.
+            doc.SaveToFile("MyFirstPDF.pdf");
+            doc.Close();
+            MessageBox.Show("ok");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PdfDocument doc = new PdfDocument();
+            doc.LoadFromFile("MyFirstPDF.pdf");
+
+            doc.Bookmarks.Clear();
+            doc.SaveToFile("MyFirstPDF.pdf");
+            doc.Dispose();
+            MessageBox.Show("ok");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PdfDocument doc = new PdfDocument();
+            doc.LoadFromFile("MyFirstPDF.pdf");
+            for (int i = 0; i < doc.Pages.Count; i++)
+            {
+                PdfBookmark bookmark = doc.Bookmarks.Add("the" + i.ToString() + "page");//添加书签标题
+                bookmark.Destination= new PdfDestination(doc.Pages[i],new PointF(0,0));//设置目的地
+               
+                bookmark.DisplayStyle = PdfTextStyle.Bold;
+                bookmark.Color = Color.Black;
+
+                //设置子目录
+                //for (int j = 0; j < 5; j++)
+                //{
+                //    PdfBookmark childbookmark = bookmark.Insert(j, "the" + j.ToString() + "page");
+                //    childbookmark.Destination = new PdfDestination(doc.Pages[i],new PointF(0, 0));
+                //    childbookmark.Destination.Location = new PointF(0, 0);
+                //    childbookmark.DisplayStyle = PdfTextStyle.Bold;
+                //    childbookmark.Color = Color.Blue;
+                //}
+            }
+            doc.SaveToFile("MyFirstPDF.pdf");
+            doc.Dispose();
+            MessageBox.Show("ok");
+
+        }
+
+   
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // 读取pdf
+            PdfDocument doc = new PdfDocument();
+            doc.LoadFromFile("基础2000试题.pdf", FileFormat.PDF);
+
+            // 读取书签txt
+            string[] lines = System.IO.File.ReadAllLines(@"书签.txt");
+            int bookmark第一级 = -1;
+            int bookmark第二级 = -1;
+            foreach (string line in lines)
+            {
+                string[] s = line.Split('\t');
+                if (s.Length == 2)
+                {
+                    // 查找
+                    PdfTextFindCollection findCollection1 = doc.Pages[int.Parse(s[1])-1].FindText(s[0]);
+                    for (int i = 0; i < findCollection1.Finds.Length; i++)
+                    {
+                        Console.WriteLine(line);
+                        // 添加书签
+                        PdfBookmark bookmark = doc.Bookmarks.Add(s[0]);//添加书签标题
+                        bookmark.Destination = new PdfDestination(doc.Pages[int.Parse(s[1])-1], new PointF(findCollection1.Finds[i].Position.X, findCollection1.Finds[i].Position.Y - 43));//设置目的地
+                        //bookmark.DisplayStyle = PdfTextStyle.Bold;
+                        //bookmark.Color = Color.Black;
+                    }
+                    bookmark第一级++;
+                }
+                if (s.Length == 3)
+                {
+                    // 查找
+                    PdfTextFindCollection findCollection1 = doc.Pages[int.Parse(s[2]) - 1].FindText(s[1]);
+                    for (int i = 0; i < findCollection1.Finds.Length; i++)
+                    {
+                        Console.WriteLine(line);
+                        // 添加书签
+                        PdfBookmark bookmark = doc.Bookmarks[bookmark第一级].Add(s[1]);//添加书签标题
+                        bookmark.Destination = new PdfDestination(doc.Pages[int.Parse(s[2]) - 1], new PointF(findCollection1.Finds[i].Position.X, findCollection1.Finds[i].Position.Y - 43));//设置目的地
+                        //bookmark.DisplayStyle = PdfTextStyle.Bold;
+                        //bookmark.Color = Color.Black;
+                    }
+                    bookmark第二级++;
+                }
+                if (s.Length == 4)
+                {
+                    // 查找
+                    PdfTextFindCollection findCollection1 = doc.Pages[int.Parse(s[3]) - 1].FindText(s[2]);
+                    for (int i = 0; i < findCollection1.Finds.Length; i++)
+                    {
+                        Console.WriteLine(line);
+                        // 添加书签
+                        PdfBookmark bookmark = doc.Bookmarks[bookmark第一级][bookmark第二级].Add(s[2]);//添加书签标题
+                        bookmark.Destination = new PdfDestination(doc.Pages[int.Parse(s[3]) - 1], new PointF(findCollection1.Finds[i].Position.X, findCollection1.Finds[i].Position.Y - 43));//设置目的地
+                        //bookmark.DisplayStyle = PdfTextStyle.Bold;
+                        //bookmark.Color = Color.Black;
+                    }
+                }
+            }
+
+            // 保存
+            doc.SaveToFile("2.pdf");
+            doc.Dispose();
+            MessageBox.Show("ok");
+            System.Diagnostics.Process.Start("2.pdf");
+        }
+    }
+}
